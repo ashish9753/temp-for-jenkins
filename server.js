9aec9dd1005a9550708f940877d0d397f93af2a6
ashish9753 <ashishs8927@gmail.com>
2026-08-24 18:40:40 +0530

    update cnanges

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-console.log("hello from Server.....");
-console.("created By Ashish")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        message: "hello from server created by Ashish Sharma"
+    })
+})
+
+app.listen(3001, ()=>{
+    console.log("server is running on Port 3001");
+})
